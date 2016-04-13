3b3102ed715712f2b6294ba061c11eb8e53050b2
Antoine BERNIER <antoine.bernier@gmail.com>
2016-04-13 11:41:09 +0200

    Update srcset.js

--- js/srcset.js
+++ js/srcset.js
@@ -1,4 +1,23 @@
 (function () {
+  //
+  // CustomEvent polyfill (https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill)
+  //
+  
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+(function () {
   var ViewportInfo = this.ViewportInfo || require('./viewport-info');
   var SrcsetInfo = this.SrcsetInfo || require('./srcset-info');
   var WeakMap = require('weak-map');
